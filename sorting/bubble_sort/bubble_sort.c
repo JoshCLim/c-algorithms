600743f48a989365b4b5aaf7efa9d03af39e14a1
Josh <joshconstantinelim@gmail.com>
2022-08-26 01:36:13 +1000

    joshL/bubble_sort-01 (#2)

---
+++ sorting/bubble_sort/bubble_sort.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+#define MAX 1000
+#define TRUE 1
+#define FALSE 0
+
+void bubble_sort(int *nums, int size);
+
+int main(void) {
+    int nums[MAX];
+
+    int curr;
+    int count = 0;
+
+    while (scanf("%d", &curr) != EOF && count < MAX) {
+        nums[count] = curr;
+        count++;
+    }
+
+    bubble_sort(nums, count);
+
+    for (int i = 0; i < count; i++) {
+        printf("%d\n", nums[i]);
+    }
+
+    return 0;
+}
+
+void bubble_sort(int *nums, int size) {
+    int sorted = FALSE;
+    while (sorted == FALSE) {
+        sorted = TRUE;
+        for (int i = 0; i < size - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
+                int tmp = nums[i];
+                nums[i] = nums[i + 1];
+                nums[i + 1] = tmp;
+                sorted = FALSE;
+            }
+        }
+    }
+}
