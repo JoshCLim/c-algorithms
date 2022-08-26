774cf6c67f67efe4cfd4a9ae086f13d3ae66b05a
Josh <joshconstantinelim@gmail.com>
2022-08-26 21:54:25 +1000

    copy files from standard BST to avl tree

---
+++ binary_search_trees/avl_trees/Tree.c
@@ -0,0 +1,157 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "Tree.h"
+
+typedef struct Node {
+    Item data;
+    struct Node *left;
+    struct Node *right;
+} Node;
+
+// ---- function prototypes ---- //
+Tree tree_create(Item);
+Tree tree_delete(Tree);
+Tree tree_insert(Tree, Item);
+Tree tree_remove(Tree, Item);
+bool tree_search(Tree, Item);
+void tree_show(Tree);
+void tree_show_formatted(Tree, int);
+void tree_list(Tree);
+void tree_list_formatted(Tree);
+Tree delete_node(Tree);
+Item find_min_value(Tree);
+
+// ---- interface functions ---- //
+Tree tree_create(Item item) {
+    Tree new_node = (Tree) malloc(sizeof(Node));
+    new_node->data = item;
+    new_node->left = NULL;
+    new_node->right = NULL;
+
+    return new_node;
+}
+
+Tree tree_delete(Tree t) {
+    if (t != NULL) {
+        tree_delete(t->left);
+        tree_delete(t->right);
+        free(t);
+    }
+    return NULL;
+}
+
+Tree tree_insert(Tree t, Item item) {
+    if (t == NULL) { // if curr value == NULL, insert here
+        Tree new_node = (Tree) malloc(sizeof(Node));
+        new_node->data = item;
+        new_node->left = NULL;
+        new_node->right = NULL;
+
+        t = new_node;
+    } else if (item < t->data) { // else if curr value bigger than value, go left (and repeat/recurse)
+        t->left = tree_insert(t->left, item);
+    } else if (item > t->data) { // else if curr value smaller than value, go right (and repeat/recurse)
+        t->right = tree_insert(t->right, item);
+    } // else if curr value == value, already in tree so do nothing
+
+    return t;
+}
+
+Tree tree_remove(Tree t, Item item) {
+    // find the node with item
+    if (t == NULL) {
+        return NULL;
+    } else if (item < t->data) {
+        t->left = tree_remove(t->left, item);
+    } else if (item > t->data) {
+        t->right = tree_remove(t->right, item);
+    } else {
+        // once we find it, delete it
+        t = delete_node(t);
+    }
+    return t;
+}
+
+bool tree_search(Tree t, Item item) {
+    if (t == NULL) {
+        return false;
+    } else if (item < t->data) {
+        return tree_search(t->left, item);
+    } else if (item > t->data) {
+        return tree_search(t->right, item);
+    }
+    return true;
+}
+
+void tree_show(Tree t) {
+    if (t == NULL) {
+        printf("<no tree created>\n");
+        return;
+    }
+    printf("\n");
+    tree_show_formatted(t, 0);
+}
+
+void tree_list(Tree t) {
+	//printf("\n");
+    //printf("  ");
+    tree_list_formatted(t);
+    printf("\n");
+}
+
+// ---- helper functions ---- //
+void tree_show_formatted(Tree t, int depth) {
+    if (t != NULL) {
+        tree_show_formatted(t->right, depth + 1);
+
+        for (int i = 0; i < depth; i++) {
+            printf("\t");
+        }
+        printf("%d\n", t->data);
+
+        tree_show_formatted(t->left, depth + 1);
+    }
+}
+
+void tree_list_formatted(Tree t) {
+    if (t != NULL) {
+		tree_list_formatted(t->left);
+		printf("%d,", t->data);
+		tree_list_formatted(t->right);
+	}
+}
+
+// deletes a node and returns a pointer to the node that should take its place
+Tree delete_node(Tree t) {
+    Tree res = NULL;
+    if (t->left == NULL && t->right == NULL) {
+        res = NULL;
+        free(t);
+    } else if (t->right == NULL) {
+        res = t->left;
+        free(t);
+    } else if (t->left == NULL) {
+        res = t->right;
+        free(t);
+    } else { // if node has left and right
+        // find minimum value to the right and set the current node to it's value
+        t->data = find_min_value(t->right);
+        // delete the minimum value to the right
+        t->right = tree_remove(t->right, t->data);
+        // return the same node
+        res = t;
+    }
+    return res;
+}
+
+// returns the minimum value of a tree
+Item find_min_value(Tree t) {
+    Tree curr = t;
+    while (curr->left != NULL) {
+        curr = curr->left;
+    }
+
+    return curr->data;
+}
---
+++ binary_search_trees/avl_trees/run.c
@@ -0,0 +1,142 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+#include "Tree.h"
+
+#define HELP 'h'
+#define CREATE 'c'
+#define DELETE 'd'
+#define INSERT 'i'
+#define REMOVE 'r'
+#define SEARCH 's'
+#define SHOW 'p'
+
+#define MAX_LEN 100
+
+void help(void);
+bool interpret_input(char input[MAX_LEN], char *tree, Item *item);
+
+int main(int argc, char *argv[]) {
+
+    if (argc != 2) {
+        printf("Usage: ./run <mode: 0=testing, 1=formatted>\n");
+        return 1;
+    }
+
+    int mode = atoi(argv[1]);
+    
+    char input[MAX_LEN];
+    Tree tree = NULL;
+    Item item;
+
+    char command = HELP;
+
+    if (mode != 0) printf("Enter command: ");
+
+    while (fgets(input, MAX_LEN, stdin) != NULL) {
+        if (!interpret_input(input, &command, &item)) {
+            if (mode != 0) printf("Enter command: ");
+            continue;
+        }
+
+        if (command == HELP) {
+            help();
+        } else if (command == CREATE) {
+            tree = tree_delete(tree);
+            tree = tree_create(item);
+        } else if (command == DELETE) {
+            tree = tree_delete(tree);
+        } else if (command == INSERT) {
+            tree = tree_insert(tree, item);
+        } else if (command == REMOVE) {
+            tree = tree_remove(tree, item);
+        } else if (command == SEARCH) {
+            fputs(tree_search(tree, item) ? "true" : "false", stdout);
+            printf("\n");
+        } else if (command == SHOW) {
+            if (item == 1) tree_show(tree);
+            else if (item == 0) tree_list(tree);
+        }
+
+        if (mode != 0) printf("\n");
+        if (mode != 0) printf("Enter command: ");
+    }
+
+    if (mode != 0) printf("\n");
+    return 0;
+}
+
+void help(void) {
+    printf("\n");
+    printf("==========\n");
+    printf("Usage:\n");
+    printf("  help      h\n");
+    printf("  create    c <item>\n");
+    printf("  delete    d\n");
+    printf("  insert    i <item>\n");
+    printf("  remove    r <item>\n");
+    printf("  search    s <item>\n");
+    printf("  print     p <format: 0=list, 1=tree>\n");
+    printf("==========\n");
+    printf("\n");
+}
+
+bool interpret_input(char input[MAX_LEN], char *command, Item *item) {
+    bool invalid_input = false;
+
+    // remove newline character
+    int i = 0;
+    while (input[i] != '\n' && input[i] != '\0' && i < MAX_LEN) {
+        i++;
+    }
+    input[i] = '\0';
+
+    // read in input
+    char *val0_str = NULL;
+    char *val1_str = NULL;
+
+    // read command in
+    val0_str = strtok(input, " ");
+    if (val0_str != NULL) {
+        if (val0_str[1] != ' ' && val0_str[1] != '\0') {
+            val0_str = NULL;
+            *command = -1;
+            invalid_input = true;
+        } else {
+            *command = val0_str[0];
+        }
+    } else {
+        *command = -1;
+        invalid_input = true;
+    }
+
+    if (
+        *command == CREATE ||
+        *command == INSERT ||
+        *command == REMOVE ||
+        *command == SEARCH ||
+        *command == SHOW
+    ) {
+        // read item in
+        val1_str = strtok(NULL, " ");
+        if (val1_str != NULL) {
+            *item = atoi(val1_str);
+        } else {
+            *item = -1;
+            invalid_input = true;
+        }
+    }
+
+    if (*command == SHOW && (*item != 0 && *item != 1)) {
+        invalid_input = true;
+    }
+
+    if (invalid_input) {
+        printf("  Invalid command. Enter 'h' for help.\n\n");
+        return false;
+    }
+
+    return true;
+}
